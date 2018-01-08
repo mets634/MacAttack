ccf0988fe9d4486c9632612439d79eba0e4b43d0
Emanuel Amit <emanuel.amit@gmail.com>
2018-01-08 11:29:29 +0200

    add documentation to client and rename server

--- Communications/Client.py
+++ Communications/Client.py
@@ -5,39 +5,62 @@ from subprocess import call
 
 
 def main():
-
+    # send 'new' message to server and receive token and username
     server_message = send_message("new")
+    # extract token and username from server message
     token = re.match(".+\|token=(.+)", server_message).group(1)
     username = re.match("username=([^|]+)", server_message).group(1)
+
     print "token = " + token
     print "username = " + username
 
     while True:
-        server_message = send_message("next")
+        # send 'next' message and receive new block number
+        block_number = send_message("next")
         print server_message
-        run_script(token, username, server_message)
-        continue
+
+        # run the script with the provided username, token and block number
         print "block_number = " + block_number
 
+        # run script and get key
         key = run_script(token, username, block_number)
 
         # if key is not empty then key found
-        if key:
-            client_socket.sendall("key=" + key)
-            client_socket.close()
+        if key != 0:
+            send_message("key=" + key)
             exit(0)
 
 
 def send_message(message):
+    """
+        send message to server and get message from server
+    :param message: message to send
+    :return: message from server
+    """
+    # create socket
     client_socket = socket.socket()
+    # connect to server
     client_socket.connect(('127.0.0.1', 3626))
+    # send message
     client_socket.sendall(message)
+    # receive message
     server_message = client_socket.recv(1024)
+    # close socket
     client_socket.close()
+
     return server_message
 
+
 def run_script(token, username, block_number):
+    """
+        run the c program to find the key for the hash
+    :param token: token to break
+    :param username: username inside token
+    :param block_number: block number to process
+    :return: key; if key not found then return 0
+    """
     time.sleep(2)
+    # run program with given params
     # call(["./script", "token", "username", "block_number"])
     key = 0
     return key
